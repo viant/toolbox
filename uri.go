0c40aef93929b6331d145829f5a7ccefe637ea37
Adrian Witas <awitas@specificmedia.com>
2016-06-30 09:52:15 -0700

    Removed license from individual file, left it on LICENSE file
    renamed util to specific helper

--- uri.go
+++ uri.go
@@ -1,23 +1,3 @@
-/*
- *
- *
- * Copyright 2012-2016 Viant.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- *  use this file except in compliance with the License. You may obtain a copy of
- *  the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  License for the specific language governing permissions and limitations under
- *  the License.
- *
- */
-
-// Package toolbox - uri utilities
 package toolbox
 
 //ExtractURIParameters parses URIs to extract {<param>} defined in templateURI from requestURI, it returns extracted parameters and flag if requestURI matched templateURI
