6ebec48ac29e97e6fc0bdc5b4f3f39b4c62b1e10
Adrian Witas <awitas@specificmedia.com>
2017-02-14 12:13:57 -0800

    added test for query value

--- uri_helper_test.go
+++ uri_helper_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/viant/toolbox"
+	"net/url"
 )
 
 func TestOpenURL(t *testing.T) {
@@ -49,4 +50,19 @@ func TestOpenReaderFromURL(t *testing.T) {
 		_, _, err := toolbox.OpenReaderFromURL("abc://github.com/viant/toolbox")
 		assert.NotNil(t, err)
 	}
+}
+
+func Test_QueryValue(t *testing.T) {
+	u, err := url.Parse("http://localhost/?k1=v1&k2=2&k3=false")
+	assert.Nil(t, err)
+
+	assert.Equal(t, "v1", toolbox.QueryValue(u, "k1", "default"))
+	assert.Equal(t, "default", toolbox.QueryValue(u, "k10", "default"))
+
+	assert.Equal(t, 2, toolbox.QueryIntValue(u, "k2", 3))
+	assert.Equal(t, 3, toolbox.QueryIntValue(u, "k10", 3))
+
+	assert.Equal(t, false, toolbox.QueryBoolValue(u, "k3", true))
+	assert.Equal(t, true, toolbox.QueryBoolValue(u, "k10", true))
+
 }
