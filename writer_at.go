abe4a763f77f729d90d4c81748af48191944c926
abioous <arleta.bilous@gmail.com>
2017-06-12 15:18:00 -0700

    patched concurrent writess

--- writer_at.go
+++ writer_at.go
@@ -1,22 +1,35 @@
 package toolbox
 
+import (
+	"sync"
+)
+
 //ByteWriterAt  represents a bytes writer at
 type ByteWriterAt struct {
+	mutex *sync.Mutex
 	Buffer   []byte
 	position int
 }
 
+
 //WriteAt returns number of written bytes or error
 func (w *ByteWriterAt) WriteAt(p []byte, offset int64) (n int, err error) {
+	w.mutex.Lock()
+
+
+
+
 	if int(offset) == w.position {
 		w.Buffer = append(w.Buffer, p...)
 		w.position += len(p)
+		w.mutex.Unlock()
 		return len(p), nil
 	} else if w.position < int(offset) {
 		var diff = (int(offset) - w.position)
 		var fillingBytes = make([]byte, diff)
 		w.position += len(fillingBytes)
 		w.Buffer = append(w.Buffer, fillingBytes...)
+		w.mutex.Unlock()
 		return w.WriteAt(p, offset)
 	} else {
 		for i := 0; i < len(p); i++ {
@@ -28,6 +41,7 @@ func (w *ByteWriterAt) WriteAt(p []byte, offset int64) (n int, err error) {
 				break
 			}
 		}
+		w.mutex.Unlock()
 		return len(p), nil
 	}
 }
@@ -35,6 +49,7 @@ func (w *ByteWriterAt) WriteAt(p []byte, offset int64) (n int, err error) {
 //NewWriterAt returns a new instance of byte writer at
 func NewByteWriterAt() *ByteWriterAt {
 	return &ByteWriterAt{
+		mutex:&sync.Mutex{},
 		Buffer: make([]byte, 0),
 	}
 }
