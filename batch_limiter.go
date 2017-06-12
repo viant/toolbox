09716667f9edeadc6dcf745904818d1f2ca2d4e0
abioous <arleta.bilous@gmail.com>
2017-06-12 15:18:49 -0700

    added batch limiter

---
+++ batch_limiter.go
@@ -0,0 +1,41 @@
+package toolbox
+
+import "sync"
+
+type BatchLimiter struct {
+	queue chan uint8
+	group *sync.WaitGroup
+	Mutex *sync.RWMutex
+}
+
+func (r *BatchLimiter) Acquire() {
+	<-r.queue
+}
+
+
+func (r *BatchLimiter) Done() {
+	r.group.Done()
+	r.queue <-uint8(1)
+}
+
+
+func (r *BatchLimiter) Wait() {
+	r.group.Wait()
+}
+
+func NewBatchLimiter(batchSize, total int) *BatchLimiter {
+	var queue = make(chan uint8, batchSize)
+	for i := 0; i< batchSize;i++ {
+		queue <- uint8(1)
+	}
+	result := &BatchLimiter{
+		queue: queue,
+		group: &sync.WaitGroup{},
+		Mutex: &sync.RWMutex{},
+	}
+
+	result.group.Add(total)
+	return result
+}
+
+
---
+++ batch_limiter_test.go
@@ -0,0 +1,30 @@
+package toolbox_test
+
+import (
+	"testing"
+	"github.com/viant/toolbox"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBatchLimiter(t *testing.T) {
+	var numbers = []int{1, 4, 6, 2, 5,7,5 ,5,7,2,3,5,7,6}
+	limiter:= toolbox.NewBatchLimiter(4, len(numbers))
+	var sum int32 = 0;
+	for _, n :=  range numbers {
+		go func(n int32) {
+			limiter.Acquire()
+			defer limiter.Done()
+			limiter.Mutex.Lock()
+			defer limiter.Mutex.Unlock()
+			//atomic.AddInt32(&sum, int32(n))
+			sum = sum + int32(n)
+		}(int32(n))
+
+	}
+	limiter.Wait()
+	var expected int32 =0;
+	for _, n:=range numbers {
+		expected += int32(n)
+	}
+	assert.Equal(t, expected, sum)
+}
