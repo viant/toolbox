733b3b71715b551f48ae0383edc35c9a980ae5d6
awitas <adrianwit@gmail.com>
2017-07-26 20:29:42 -0700

    added NewConfig func

--- storage/aws/config.go
+++ storage/aws/config.go
@@ -1,9 +1,17 @@
 package aws
 
+import "github.com/viant/toolbox"
+
 //Config represents storage
 type Config struct {
 	Region string
 	Key    string
 	Secret string
 	Token  string
+}
+
+//NewConfig creates a new config from URL
+func NewConfig(URL string) (*Config, error) {
+	var result = &Config{}
+	return result, toolbox.LoadConfigFromUrl(URL, result)
 }
