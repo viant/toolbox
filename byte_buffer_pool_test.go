70dccd7896d3860ee5a16fc2fbad71268c62026a
Adrian Witas <adrianwitas@Adrians-MacBook-Pro.local>
2017-08-21 12:45:39 -0700

    added buffer pool

---
+++ byte_buffer_pool_test.go
@@ -0,0 +1,27 @@
+package toolbox_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/viant/toolbox"
+	"testing"
+)
+
+func TestNewBytesBufferPool(t *testing.T) {
+	pool := toolbox.NewBytesBufferPool(2, 1024)
+	buf := pool.Get() //creates a new buffer pool is empty.
+	assert.NotNil(t, buf)
+
+	buf[1] = 0x2
+	pool.Put(buf)
+	pool.Put([]byte{0x1})
+	pool.Put(buf) //get discarded
+	{
+		poolBuf := pool.Get()
+		assert.Equal(t, uint8(0x2), poolBuf[1])
+		assert.Equal(t, 1024, len(poolBuf))
+	}
+	{
+		poolBuf := pool.Get()
+		assert.Equal(t, 1, len(poolBuf))
+	}
+}
