0a1f0a66132224825ad1dbf3d1533c7ffc7008e7
adrianwit <adrianwit@gmail.com>
2017-11-05 19:51:33 -0800

    patched file info on linux

--- storage/scp/service_test.go
+++ storage/scp/service_test.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"github.com/viant/toolbox/storage"
 )
 
 func TestService_List(t *testing.T) {
@@ -53,5 +54,24 @@ func TestService_Delete(t *testing.T) {
 
 	err = service.Delete(object)
 	assert.Nil(t, err)
+
+}
+
+func Test_ExtractFileInfo(t *testing.T) {
+
+	{
+		var object storage.Object = scp.ExtractFileInfo("-rw-r--r--  1 awitas  1742120565   414 Jun  8 14:14:08 2017 f.pub", false)
+		assert.Equal(t, "/f.pub", object.URL())
+		assert.Equal(t, int64(1496931248), object.LastModified().Unix())
+		assert.Equal(t, int64(414), object.Size())
+		assert.Equal(t, true, object.IsContent())
+	}
+
+
+	var object storage.Object = scp.ExtractFileInfo("-rw-r--r-- 1 awitas awitas 2002 2017-11-04 22:29:33.363458941 +0000 aerospikeciads_aerospike.conf", true)
+	assert.Equal(t, "/aerospikeciads_aerospike.conf", object.URL())
+	assert.Equal(t, int64(1509834573), object.LastModified().Unix())
+	assert.Equal(t, int64(2002), object.Size())
+	assert.Equal(t, true, object.IsContent())
 
 }
