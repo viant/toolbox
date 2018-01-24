8d92cfe91e82ad4bb6e5059adda4250758e8ee3b
adrianwit <adrianwit@gmail.com>
2018-01-24 09:49:12 -0800

    updated init empty struct

--- struct_helper.go
+++ struct_helper.go
@@ -3,6 +3,7 @@ package toolbox
 import (
 	"reflect"
 	"strings"
+	"fmt"
 )
 
 const (
@@ -94,4 +95,95 @@ func BuildTagMapping(structTemplatePointer interface{}, mappedKeyTag string, res
 //NewFieldSettingByKey reads field's tags and returns them indexed by passed in key, fieldName is always part of the resulting map unless filed has "transient" tag.
 func NewFieldSettingByKey(aStruct interface{}, key string) map[string](map[string]string) {
 	return BuildTagMapping(aStruct, key, "transient", true, true, columnMapping)
+}
+
+
+func setEmptyMap(source reflect.Value) {
+	mapType := source.Type()
+	mapPointer := reflect.New(mapType)
+	mapValueType := mapType.Elem()
+	mapKeyType := mapType.Key()
+	newMap := mapPointer.Elem()
+	newMap.Set(reflect.MakeMap(mapType))
+	targetMapKeyPointer := reflect.New(mapKeyType)
+	targetMapValuePointer := reflect.New(mapValueType)
+	var elementKey = targetMapKeyPointer.Elem()
+	var elementValue = targetMapValuePointer.Elem()
+	if elementKey.Type() != mapKeyType {
+		if elementKey.Type().AssignableTo(mapKeyType) {
+			elementKey = elementKey.Convert(mapKeyType)
+		}
+	}
+	if DereferenceType(elementValue.Type()).Kind() == reflect.Struct {
+		InitStruct(elementValue.Interface())
+	}
+	newMap.SetMapIndex(elementKey, elementValue)
+	source.Set(mapPointer.Elem())
+}
+
+
+func createEmptySlice(source reflect.Value) {
+
+	sliceType := DiscoverTypeByKind(source.Type(), reflect.Slice)
+	slicePointer := reflect.New(sliceType)
+	slice := slicePointer.Elem()
+	componentType := DiscoverComponentType(sliceType)
+
+	var targetComponentPointer = reflect.New(componentType)
+	var targetComponent = targetComponentPointer.Elem()
+	if DereferenceType(componentType).Kind() == reflect.Struct {
+		structElement := reflect.New(targetComponent.Type().Elem())
+		InitStruct(structElement.Interface())
+		targetComponentPointer.Elem().Set(structElement)
+		InitStruct(targetComponentPointer.Elem().Interface())
+	}
+	slice.Set(reflect.Append(slice, targetComponentPointer.Elem()))
+	source.Set(slicePointer.Elem())
+
+
+}
+
+//InitStruct initialise any struct pointer to empty struct
+func InitStruct(source interface{}) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Recovered %v\n", r)
+		}
+	}()
+	if ! IsStruct(source) {
+		return
+	}
+	sourceValue, ok := source.(reflect.Value)
+	if ! ok {
+		sourceValue = reflect.ValueOf(source)
+	}
+	structValue := DiscoverValueByKind(sourceValue, reflect.Struct)
+	structType := structValue.Type()
+	for i := 0; i < structType.NumField(); i++ {
+		fieldValue := structValue.Field(i)
+		if ! fieldValue.CanInterface() {
+			continue
+		}
+		fieldType := structType.Field(i)
+		if fieldType.Type.Kind() == reflect.Map   {
+			setEmptyMap(fieldValue)
+			continue
+		}
+		if fieldType.Type.Kind() == reflect.Slice   {
+			createEmptySlice(fieldValue)
+			continue
+		}
+
+		if fieldType.Type.Kind() != reflect.Ptr {
+			continue
+		}
+
+		if DereferenceType(fieldType).Kind() == reflect.Struct {
+			fieldStruct := reflect.New(fieldValue.Type().Elem())
+			InitStruct(fieldStruct.Interface())
+			fieldValue.Set(fieldStruct)
+		}
+
+	}
+
 }
