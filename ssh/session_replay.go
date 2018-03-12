6814c49df5bbf632c582026ad74751a988566928
adrianwit <adrianwit@gmail.com>
2018-03-11 22:58:23 -0700

    rename private filed

--- ssh/session_replay.go
+++ ssh/session_replay.go
@@ -3,28 +3,28 @@ package ssh
 import (
 	"errors"
 	"strings"
-	)
+)
 
 const commandNotFound = "Command not found"
 
 type replayMultiCommandSession struct {
 	shellPrompt string
 	system      string
-	commands    *ReplayCommands
+	replay      *ReplayCommands
 }
 
 func (s *replayMultiCommandSession) Run(command string, timeoutMs int, terminators ...string) (string, error) {
 	if !strings.HasSuffix(command, "\n") {
 		command = command + "\n"
 	}
-	replay, ok := s.commands.Commands[command]
+	replay, ok := s.replay.Commands[command]
 	if !ok {
 		return commandNotFound, nil
 	}
 	if replay.Error != "" {
 		return "", errors.New(replay.Error)
 	}
-	return s.commands.Next(command), nil
+	return s.replay.Next(command), nil
 }
 
 func (s *replayMultiCommandSession) ShellPrompt() string {
@@ -43,6 +43,6 @@ func NewReplayMultiCommandSession(shellPrompt, system string, commands *ReplayCo
 	return &replayMultiCommandSession{
 		shellPrompt: shellPrompt,
 		system:      system,
-		commands:    commands,
+		replay:      commands,
 	}
 }
