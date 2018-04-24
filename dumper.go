55b7974a63ea7f14729aee05f4136afa829ffc84
adrianwit <adrianwit@gmail.com>
2018-04-24 10:23:21 -0700

    added utiliy method

--- dumper.go
+++ dumper.go
@@ -9,3 +9,24 @@ func Dump(data interface{}) {
 		return
 	}
 }
+
+//DumpIndent prints passed in data as indented JSON
+func DumpIndent(data interface{}, removeEmptyKeys bool) error {
+	if IsMap(data) || IsStruct(data) {
+		var aMap= map[string]interface{}{}
+		if err := DefaultConverter.AssignConverted(&aMap, data);err != nil {
+			return err
+		}
+		data = aMap
+		if removeEmptyKeys {
+			data = DeleteEmptyKeys(aMap)
+		}
+	}
+
+	text, err := AsIndentJSONText(data);
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", text)
+	return nil
+}
