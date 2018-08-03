61f235cb92b86ae9258c287966ab7c9ceb9e8471
adrianwit <adrianwit@gmail.com>
2018-08-03 19:36:56 +0200

    update http service with prive git scraping

--- storage/http_service.go
+++ storage/http_service.go
@@ -54,6 +54,7 @@ func extractLinks(body string) []*hRef {
 		if linkEndPosition == -1 {
 			continue
 		}
+		linkHref :=string(linkContent[:linkEndPosition] )
 		var content = ""
 		contentStartPosition := strings.Index(linkContent, ">")
 		if contentStartPosition != 1 {
@@ -63,9 +64,8 @@ func extractLinks(body string) []*hRef {
 				content = string(content[:contentEndPosition])
 			}
 		}
-
 		link := &hRef{
-			URL:   string(linkContent[:linkEndPosition]),
+			URL:   linkHref,
 			Value: strings.Trim(content, " \t\r\n"),
 		}
 		result = append(result, link)
@@ -100,35 +100,52 @@ func (s *httpStorageService) List(URL string) ([]Object, error) {
 		return nil, fmt.Errorf("Invalid response code: %v", response.Status)
 	}
 
-	isGitUrl := strings.Contains(URL, "github.com")
+	isGitUrl := strings.Contains(URL, "github.")
+	isPublicGit  := strings.Contains(URL, "github.com")
 	if strings.Contains(contentType, "text/html") {
 
 		links := extractLinks(string(body))
-
+		var indexedLinks = map[string]bool{}
 		if isGitUrl {
 
 			for _, link := range links {
 				if !((strings.Contains(link.URL, "/blob/") || strings.Contains(link.URL, "/tree/")) && strings.HasSuffix(link.URL, link.Value)) {
 					continue
 				}
-
 				linkType := StorageObjectContentType
-				if strings.Contains(link.URL, "/tree/") {
+				_, name := toolbox.URLSplit(link.URL)
+				if path.Ext(name) == "" {
 					linkType = StorageObjectFolderType
 				}
 
 				baseURL := toolbox.URLBase(URL)
-				objectURL := toolbox.URLPathJoin(baseURL, link.URL)
 
-				if linkType == StorageObjectContentType {
+				objectURL := link.URL
+				if ! strings.Contains(objectURL, baseURL) {
+					objectURL = toolbox.URLPathJoin(baseURL, link.URL)
+				}
+
+				if linkType == StorageObjectContentType && strings.Contains(objectURL, "/master/") {
 					objectURL = strings.Replace(objectURL, "/blob/", "/", 1)
-					objectURL = strings.Replace(objectURL, "github.com", "raw.githubusercontent.com", 1)
+					if isPublicGit {
+						objectURL = strings.Replace(objectURL, "github.com", "raw.githubusercontent.com", 1)
+					} else {
+						objectURL = strings.Replace(objectURL, ".com/", ".com/raw/", 1)
+					}
+				}
+				if linkType ==  StorageObjectContentType && !strings.Contains(objectURL, "raw") {
+					continue
+				}
+				if _, ok:= indexedLinks[objectURL];ok {
+					continue
 				}
 				storageObject := newHttpFileObject(objectURL, linkType, nil, now, 1)
+				indexedLinks[objectURL]=true
 				result = append(result, storageObject)
 			}
 
 		} else {
+
 			for _, link := range links {
 				if link.URL == "" || strings.Contains(link.URL, ":") || strings.HasPrefix(link.URL, "#") || strings.HasPrefix(link.URL, "?") || strings.HasPrefix(link.URL, ".") || strings.HasPrefix(link.URL, "/") {
 					continue
