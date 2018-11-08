0fd952123082c2d231832462e7537ed293663bc1
adrianwit <adrianwit@gmail.com>
2018-11-08 11:49:08 -0800

    patched waitgroup helper

--- waitgroup_helper.go
+++ waitgroup_helper.go
@@ -8,8 +8,7 @@ import (
 // WaitGroup that waits with a timeout
 // Returns true if timeout exceeded and false if there was no timeout
 func WaitTimeout(wg *sync.WaitGroup, duration time.Duration) bool {
-	done := make(chan struct{})
-
+	done := make(chan struct{}, 0)
 	go func() {
 		defer close(done)
 		wg.Wait()
@@ -17,9 +16,9 @@ func WaitTimeout(wg *sync.WaitGroup, duration time.Duration) bool {
 
 	select {
 	case <-done: //Wait till the task is complete and channel get unblocked
-		return false //No duration. Normal execution of task completion
-	case <-time.After(duration): //Wait till duration to elapse
-		//TODO: time.After() creates a timer that does not get GC until timer duration gets elapsed. Need to use AfterFunc
+		return false //No durationToken. Normal execution of task completion
+	case <-time.After(duration): //Wait till durationToken to elapse
+		//TODO: time.After() creates a timer that does not get GC until timer durationToken gets elapsed. Need to use AfterFunc
 		return true //Timed out
 	}
 }
