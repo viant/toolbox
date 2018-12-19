c50d9e053797be87119a21048db3ed5b566facda
adrianwit <adrianwit@gmail.com>
2018-12-18 19:46:36 -0800

    added NormalizeKVPairs

--- yaml.go
+++ yaml.go
@@ -15,3 +15,47 @@ func AsYamlText(source interface{}) (string, error) {
 	}
 	return "", fmt.Errorf("unsupported type: %T", source)
 }
+
+//NormalizeKVPairs converts slice of KV paris into a map, and map[interface{}]interface{} to map[string]interface{}
+func NormalizeKVPairs(source interface{}) (interface{}, error) {
+	isDataStruct := IsMap(source) || IsStruct(source) ||  IsSlice(source)
+	var err error
+	var normalized interface{}
+	if isDataStruct {
+		var aMap = make(map[string]interface{})
+		if err = ProcessMap(source, func(k, value interface{}) bool {
+			var key = AsString(k)
+			aMap[key] = value
+			if IsMap(value) || IsSlice(value) || IsStruct(value) {
+					if normalized, err = NormalizeKVPairs(value);err == nil {
+						aMap[key] = normalized
+					}
+			}
+			return true
+		});err == nil {
+			return aMap, nil
+		}
+		if IsSlice(source) { //yaml style map conversion if applicable
+			aSlice := AsSlice(source)
+			if len(aSlice) == 0 {
+				return source, nil
+			}
+			if IsMap(aSlice[0]) || IsStruct(aSlice[0]) {
+				if item, err := NormalizeKVPairs(aSlice[0]);err == nil {
+					return []interface{}{item}, nil
+				}
+
+			} else if IsSlice(aSlice[0]) {
+				for i, item := range aSlice {
+					if normalized, err = NormalizeKVPairs(item);err == nil {
+						aSlice[i] = normalized
+					} else {
+						return nil, err
+					}
+				}
+			}
+			return aSlice, nil
+		}
+	}
+	return source, err
+}
---
+++ yaml_test.go
@@ -0,0 +1,69 @@
+package toolbox
+
+import (
+	"github.com/gin-gonic/gin/json"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+	"strings"
+	"testing"
+)
+
+func TestNormalizeKVPairs(t *testing.T) {
+
+	{//yaml case
+		YAML := `- Requests:
+    - URL: http://localhost:5000
+      Method: GET
+      Header:
+        aHeader:
+          - "v1"
+          - "v2"
+
+        someOtherHeader:
+
+          - "CP=RTO"
+
+      Body: "hey there"
+      Cookies:
+        - Name: aHeader
+          Value: a-value
+          DYAMLomain: "localhost"
+          Expires: "2023-12-16T20:17:38Z"
+          RawExpires: Sat, 16 Dec 2023 20:17:38 GMT`
+
+		var data interface{}
+		err := yaml.NewDecoder(strings.NewReader(YAML)).Decode(&data);
+		assert.Nil(t, err)
+		normalized, err := NormalizeKVPairs(data)
+		assert.Nil(t, err)
+		requests := AsMap(AsSlice(normalized)[0])["Requests"]
+		request := AsMap(AsSlice(requests)[0])
+		assert.Equal(t, "http://localhost:5000", request["URL"])
+		header := AsMap(request["Header"])
+		assert.Equal(t, []interface{}{"v1", "v2"}, header["aHeader"])
+	}
+	{
+		JSON :=`[
+{"Key":"k1", "Value":"v1"},
+{"Key":"k2", "Value":"v2"},
+{"Key":"k3", "Value":[
+	{"Key":"k1", "Value":"v1", "Attr":2}
+]}]`
+
+		var data interface{}
+		err := json.NewDecoder(strings.NewReader(JSON)).Decode(&data)
+		assert.Nil(t, err)
+		normalized, err := NormalizeKVPairs(data)
+		assert.Nil(t, err)
+		aMap := AsMap(normalized)
+		assert.Equal(t, "v1", aMap["k1"])
+		assert.Equal(t, "v2", aMap["k2"])
+		aSlice := AsSlice(aMap["k3"])
+		assert.NotNil(t, aSlice)
+		anItem := AsMap(aSlice[0])
+		assert.Equal(t, "k1", anItem["Key"])
+		assert.Equal(t, "v1", anItem["Value"])
+		assert.Equal(t, 2.0, anItem["Attr"])
+	}
+
+}
