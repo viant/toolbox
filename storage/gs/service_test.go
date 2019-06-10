1be8e4d172138324f40d55ea61a2aeab0c5ce864
Adrian Witas <adrianwit@gmail.com>
2019-06-10 07:59:15 -0700

    commented test

--- storage/gs/service_test.go
+++ storage/gs/service_test.go
@@ -1,24 +1,12 @@
 package gs
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/viant/toolbox/storage"
-	"log"
-	"os"
 	"testing"
 )
 
 func TestService_List(t *testing.T) {
 
 
-	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "/Users/awitas/.secret/viant-e2e.json")
-	service, err := storage.NewServiceForURL("gs://doo/", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	reader, err := service.DownloadWithURL("gs://e2e-siteprofile-indexer/input/app_rules.csv")
-	assert.Equal(t, reader, err)
 
 
 	/*	credential := option.WithServiceAccountFile("<Secret file path>")
