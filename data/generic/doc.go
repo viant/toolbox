a37c7744b91fbc5b61d850b357b466352485003c
adrianwit <adrianwit@gmail.com>
2020-01-19 17:51:02 -0800

    updated 500 handling

--- data/generic/doc.go
+++
@@ -1,6 +0,0 @@
-//package dynamic defines dynamic data type
-/*
-To reduce memory footprint and avoid reflect slice position based storage is used with shared meta object.
-Meta object controls conversion between field and field position withing given object, slice item
- */
-package dynamic
