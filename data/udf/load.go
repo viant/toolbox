90fdd978816aeb3f2a0da699f30d1053069e96b6
Adrian Witas <adrianwit@gmail.com>
2020-05-16 11:35:45 -0700

    updated udf

--- data/udf/load.go
+++ data/udf/load.go
@@ -19,7 +19,22 @@ func LoadJSON(source interface{}, state data.Map) (interface{}, error) {
 	}
 	JSON := string(data)
 	if toolbox.IsNewLineDelimitedJSON(JSON) {
-		return toolbox.NewLineDelimitedJSON(JSON)
+		slice, err :=  toolbox.NewLineDelimitedJSON(JSON)
+		if err != nil {
+			return nil, err
+		}
+		var result = make([]interface{}, 0)
+		toolbox.ProcessSlice(slice, func(item interface{}) bool {
+			if item == nil {
+				return true
+			}
+			if toolbox.IsMap(item) && len(toolbox.AsMap(item)) == 0 {
+				return true
+			}
+			result = append(result, item)
+			return true
+		})
+		return result, nil
 	}
 	var result interface{}
 	err = json.Unmarshal(data, &result)
