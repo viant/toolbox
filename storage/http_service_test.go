e2167d336c440cb4c254cc5a5f98ccf4de7ed56b
Adrian Witas <adrianwit@gmail.com>
2020-06-25 09:02:16 -0700

    updated test

--- storage/http_service_test.go
+++ storage/http_service_test.go
@@ -1,30 +1,25 @@
 package storage_test
 
-import (
-	"github.com/stretchr/testify/assert"
-	"github.com/viant/toolbox/storage"
-	"io/ioutil"
-	"testing"
-)
+import "testing"
 
 func TestNewHttpStorageService(t *testing.T) {
-	credentialFile := ""
 
-	{
-		service, err := storage.NewServiceForURL("https://github.com/viant/", credentialFile)
-		assert.Nil(t, err)
-		assert.NotNil(t, service)
-
-		objects, err := service.List("https://github.com/viant/")
-		assert.True(t, len(objects) > 0)
-		reader, err := service.Download(objects[0])
-		if assert.Nil(t, err) {
-			defer reader.Close()
-		}
-		content, err := ioutil.ReadAll(reader)
-		assert.Nil(t, err)
-		assert.True(t, len(content) > 0)
-
-	}
+	// Deprecated use github/viant/afs/http
+	//{
+	//	service, err := storage.NewServiceForURL("https://github.com/viant/", credentialFile)
+	//	assert.Nil(t, err)
+	//	assert.NotNil(t, service)
+	//
+	//	objects, err := service.List("https://github.com/viant/")
+	//	assert.True(t, len(objects) > 0)
+	//	reader, err := service.Download(objects[0])
+	//	if assert.Nil(t, err) {
+	//		defer reader.Close()
+	//	}
+	//	content, err := ioutil.ReadAll(reader)
+	//	assert.Nil(t, err)
+	//	assert.True(t, len(content) > 0)
+	//
+	//}
 
 }
