4a7e67cca580735929efc67fcfe4504927400f74
adranwit <adrianwit@gmail.com>
2026-01-07 12:21:40 -0800

    patched nil pointer

--- types.go
+++ types.go
@@ -6,13 +6,13 @@ import (
 	"time"
 )
 
-//Zeroable represents object that can call IsZero
+// Zeroable represents object that can call IsZero
 type Zeroable interface {
 	//IsZero returns true, if value of object was zeroed.
 	IsZero() bool
 }
 
-//IsInt returns true if input is an int
+// IsInt returns true if input is an int
 func IsInt(input interface{}) bool {
 	switch input.(type) {
 	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
@@ -21,12 +21,12 @@ func IsInt(input interface{}) bool {
 	return false
 }
 
-//IsNumber returns true if type is either float or int
+// IsNumber returns true if type is either float or int
 func IsNumber(input interface{}) bool {
 	return IsFloat(input) || IsInt(input)
 }
 
-//IsFloat returns true if input is a float
+// IsFloat returns true if input is a float
 func IsFloat(input interface{}) bool {
 	switch input.(type) {
 	case float32, float64:
@@ -35,7 +35,7 @@ func IsFloat(input interface{}) bool {
 	return false
 }
 
-//IsBool returns true if input is a boolean
+// IsBool returns true if input is a boolean
 func IsBool(input interface{}) bool {
 	switch input.(type) {
 	case bool:
@@ -44,7 +44,7 @@ func IsBool(input interface{}) bool {
 	return false
 }
 
-//IsString returns true if input is a string
+// IsString returns true if input is a string
 func IsString(input interface{}) bool {
 	switch input.(type) {
 	case string:
@@ -53,12 +53,12 @@ func IsString(input interface{}) bool {
 	return false
 }
 
-//CanConvertToString checks if  input can be converted to string
+// CanConvertToString checks if  input can be converted to string
 func CanConvertToString(input interface{}) bool {
 	return reflect.TypeOf(input).AssignableTo(reflect.TypeOf(""))
 }
 
-//IsTime returns true if input is a time
+// IsTime returns true if input is a time
 func IsTime(input interface{}) bool {
 	switch input.(type) {
 	case time.Time:
@@ -69,17 +69,23 @@ func IsTime(input interface{}) bool {
 	return false
 }
 
-//IsMap returns true if input is a map
+// IsMap returns true if input is a map
 func IsMap(input interface{}) bool {
+	if input == nil {
+		return false
+	}
 	switch input.(type) {
 	case map[string]interface{}:
 		return true
 	}
 	candidateType := DereferenceType(reflect.TypeOf(input))
+	if candidateType == nil {
+		return false
+	}
 	return candidateType.Kind() == reflect.Map
 }
 
-//IsStruct returns true if input is a map
+// IsStruct returns true if input is a map
 func IsStruct(input interface{}) bool {
 	if input == nil {
 		return false
@@ -88,7 +94,7 @@ func IsStruct(input interface{}) bool {
 	return inputType.Kind() == reflect.Struct
 }
 
-//IsSlice returns true if input is a map
+// IsSlice returns true if input is a map
 func IsSlice(input interface{}) bool {
 	switch input.(type) {
 	case []interface{}:
@@ -100,13 +106,13 @@ func IsSlice(input interface{}) bool {
 	return candidateType.Kind() == reflect.Slice
 }
 
-//IsFunc returns true if input is a funct
+// IsFunc returns true if input is a funct
 func IsFunc(input interface{}) bool {
 	candidateType := DereferenceType(reflect.TypeOf(input))
 	return candidateType.Kind() == reflect.Func
 }
 
-//IsZero returns true if input is a zeroable
+// IsZero returns true if input is a zeroable
 func IsZero(input interface{}) bool {
 	if zeroable, ok := input.(Zeroable); ok {
 		return zeroable.IsZero()
@@ -114,7 +120,7 @@ func IsZero(input interface{}) bool {
 	return false
 }
 
-//IsPointer returns true if input is a pointer
+// IsPointer returns true if input is a pointer
 func IsPointer(input interface{}) bool {
 	if reflectType, ok := input.(reflect.Type); ok {
 		return reflectType.Kind() == reflect.Ptr
@@ -122,25 +128,25 @@ func IsPointer(input interface{}) bool {
 	return reflect.TypeOf(input).Kind() == reflect.Ptr
 }
 
-//AssertPointerKind checks if input is a pointer of the passed in kind, if not it panic with message including name
+// AssertPointerKind checks if input is a pointer of the passed in kind, if not it panic with message including name
 func AssertPointerKind(input interface{}, kind reflect.Kind, name string) {
 	AssertTypeKind(reflect.TypeOf(input), reflect.Ptr, name)
 	AssertTypeKind(reflect.TypeOf(input).Elem(), kind, name)
 }
 
-//AssertKind checks if input is of the passed in kind, if not it panic with message including name
+// AssertKind checks if input is of the passed in kind, if not it panic with message including name
 func AssertKind(input interface{}, kind reflect.Kind, name string) {
 	AssertTypeKind(reflect.TypeOf(input), kind, name)
 }
 
-//AssertTypeKind checks if dataType is of the passed in kind, if not it panic with message including name
+// AssertTypeKind checks if dataType is of the passed in kind, if not it panic with message including name
 func AssertTypeKind(dataType reflect.Type, kind reflect.Kind, name string) {
 	if dataType.Kind() != kind {
 		panic(fmt.Sprintf("failed to check: %v - expected kind: %v but found %v (%v)", name, kind.String(), dataType.Kind(), dataType.String()))
 	}
 }
 
-//DiscoverValueByKind returns unwrapped input that matches expected kind, or panic if this is not possible
+// DiscoverValueByKind returns unwrapped input that matches expected kind, or panic if this is not possible
 func DiscoverValueByKind(input interface{}, expected reflect.Kind) reflect.Value {
 	result, err := TryDiscoverValueByKind(input, expected)
 	if err == nil {
@@ -149,7 +155,7 @@ func DiscoverValueByKind(input interface{}, expected reflect.Kind) reflect.Value
 	panic(err)
 }
 
-//TryDiscoverValueByKind returns unwrapped input that matches expected kind, or panic if this is not possible
+// TryDiscoverValueByKind returns unwrapped input that matches expected kind, or panic if this is not possible
 func TryDiscoverValueByKind(input interface{}, expected reflect.Kind) (reflect.Value, error) {
 	value, ok := input.(reflect.Value)
 	if !ok {
@@ -165,7 +171,7 @@ func TryDiscoverValueByKind(input interface{}, expected reflect.Kind) (reflect.V
 	return value, fmt.Errorf("failed to discover value by kind expected: %v, actual:%T   on %v:", expected.String(), value.Type(), value)
 }
 
-//IsValueOfKind returns true if passed in input is of supplied kind.
+// IsValueOfKind returns true if passed in input is of supplied kind.
 func IsValueOfKind(input interface{}, kind reflect.Kind) bool {
 	value, ok := input.(reflect.Value)
 	if !ok {
@@ -181,7 +187,7 @@ func IsValueOfKind(input interface{}, kind reflect.Kind) bool {
 	return false
 }
 
-//DiscoverTypeByKind returns unwrapped input type that matches expected kind, or panic if this is not possible
+// DiscoverTypeByKind returns unwrapped input type that matches expected kind, or panic if this is not possible
 func DiscoverTypeByKind(input interface{}, expected reflect.Kind) reflect.Type {
 	result, err := TryDiscoverTypeByKind(input, expected)
 	if err != nil {
@@ -190,7 +196,7 @@ func DiscoverTypeByKind(input interface{}, expected reflect.Kind) reflect.Type {
 	return result
 }
 
-//TryDiscoverTypeByKind returns unwrapped input type that matches expected kind, or error
+// TryDiscoverTypeByKind returns unwrapped input type that matches expected kind, or error
 func TryDiscoverTypeByKind(input interface{}, expected reflect.Kind) (reflect.Type, error) {
 	value, ok := input.(reflect.Type)
 	if !ok {
@@ -204,7 +210,7 @@ func TryDiscoverTypeByKind(input interface{}, expected reflect.Kind) (reflect.Ty
 	return nil, fmt.Errorf("failed to discover type by kind %v, on %v:", expected.String(), value)
 }
 
-//DiscoverComponentType returns type unwrapped from pointer, slice or map
+// DiscoverComponentType returns type unwrapped from pointer, slice or map
 func DiscoverComponentType(input interface{}) reflect.Type {
 	valueType, ok := input.(reflect.Type)
 	if !ok {
